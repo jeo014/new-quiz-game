7aa69fc3df62522debf5db8a4c11c5e9c443c667
Julia Ouimet <julia.ouimet@gmail.com>
2017-01-30 22:20:54 -0500

    app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-//Use a JS object to separately hold each group of: questions, choices and correct answers.
-//Use a JS function so that when <button> is clicked, it:
-//**removes the current text from the <.questions> DIV
-//**clears the radio buttons
-//**adds the next question's text from <allQuestions> to the <.questions> DIV
-//**adds the next anwers the radio buttons 
-//Use a JS object to store each of the user's answers, which are determined by which
-//radio button is selected when <button> is clicked. 
-//If user clicks <button> without first selecting a radio button, do not update the form, and
-//do not store their answer. Instead, alert the user.
-//On the final page, let the user know they are done. Tally and display the total
-//amount of correct answers. 
-
 $(function() {
 
 // display welcome text
@@ -96,11 +83,23 @@ $(function() {
 // show question number
 		$(".question-number").html("Question " + questionNumber + " of " + quizLength);
       
-// compare selected answer with correct answer
+// compare selected answer with correct answer and display result
 
 	     if ($('input[name="options"]:checked').val() == currentAnswer) {
 			score.correct++;
+			$(".js-results-message").html("Yeah! You answered the last question correctly!");
 	     }
+
+	     else {
+	     	$(".js-results-message").html("Bummer! Your last answer was incorrect :(");
+	     }
+// // but don't display the result message for the first question
+		if (questionNumber == 1){
+			$(".js-results-message").addClass("hidden");
+		}
+		else {
+			$(".js-results-message").removeClass("hidden");
+		}
 
 // show answer
          $(".answer-display").html("The answer is " + currentAnswer)
@@ -126,4 +125,4 @@ $(function() {
       location.reload();
     })
     
-})
+}) 
